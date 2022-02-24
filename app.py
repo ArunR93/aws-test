8e547530af5689be63bbabc7496be9eab4a11205
ArunkumarRajendran <arun@master-local.localdomain.com>
2022-02-24 11:19:36 +0530

    Thursday 24 February 2022 11:19:36 AM IST

--- app.py
+++ app.py
@@ -11,19 +11,5 @@ def index():
     return "Congratulations, it's a web app!"
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=8080, debug=True)
+    app.run(host="0.0.0.0", port=8080, debug=True)
 
-
-
-# from flask import Flask
-#
-# app = Flask(__name__)
-#
-#
-# @app.route('/hello/<name>')
-# def hello_name(name):
-#     return 'Hello %s!' % name
-#
-#
-# if __name__ == "__main__":
-#     app.run(host="127.0.0.1", port=8080, debug=True)
